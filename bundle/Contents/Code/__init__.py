6d33117079c636c1dbf0c3c33b1b590e75638a05
Dave Spurr <david.spurr@gmail.com>
2011-01-28 23:54:21 +0000

    - obsolete classes + spotlight section + play/pause functionality

--- bundle/Contents/Code/__init__.py
+++ bundle/Contents/Code/__init__.py
@@ -1,7 +1,3 @@
-# import re, string
-# from PMS import *
-# from PMS.Objects import *
-# from PMS.Shortcuts import *
 import htmlentitydefs
 import re
 from datetime import datetime
@@ -13,6 +9,7 @@ PLUGIN_PREFIX              = '/video/pokerstars-tv'
 # URLS
 BASE_URL                   = 'http://www.pokerstars.tv'
 CHANNELS_URL               = '%s/poker-channels' % BASE_URL
+SCHEDULE_URL               = '%s/docs-pokerstarstv-schedule.html' % BASE_URL
 
 # Default artwork and icon(s)
 PLUGIN_ARTWORK             = 'art-default.png'
@@ -20,46 +17,47 @@ PLUGIN_ICON_DEFAULT        = 'icon-default.png'
 
 ###############################################################################
 def Start():
-  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, PLUGIN_TITLE, PLUGIN_ICON_DEFAULT, PLUGIN_ARTWORK)
+  Plugin.AddPrefixHandler(PLUGIN_PREFIX, MainMenu, L('pokerstars.tv'), PLUGIN_ICON_DEFAULT, PLUGIN_ARTWORK)
   Plugin.AddViewGroup('Channels', viewMode='InfoList', mediaType='items')
   Plugin.AddViewGroup('ChannelDetails', viewMode='InfoList', mediaType='items')
   Plugin.AddViewGroup('ChannelVideos', viewMode='InfoList', mediaType='items')
-#    Plugin.AddViewGroup("Channels", viewMode="InfoList", mediaType="items")
-#    MediaContainer.title1 = L('pokerstars.tv')
-#    MediaContainer.art    = R(POKERSTARS_ART)
-#    HTTP.CacheTime        = 0 # CACHE_1HOUR
+  HTTP.CacheTime = CACHE_1HOUR
 
 ###################################################################################################
 # MENUS
 ###################################################################################################
 def MainMenu():
   dir = MediaContainer(title1=PLUGIN_TITLE,viewGroup='Channels')
+  dir.Append(
+    Function( DirectoryItem( Spotlight, 'Spotlight', thumb=R(PLUGIN_ICON_DEFAULT) ) )
+  )
+  
   channels = HTML.ElementFromURL( CHANNELS_URL, errors='ignore').xpath('//*/div[@id="template"]/ul/li/div[@class="content clearfix"]/a[@class="logo"]')
   for channel in channels:
     url      = channel.get('href')
     img      = channel.xpath('.//img')
     name     = img[0].get('alt').replace(' logo', '')
-    thumbUrl = img[0].get('src')
+    thumb_url = img[0].get('src')
     # Log( url )
     # Log( name )
-    # Log( thumbUrl ) 
+    # Log( thumb_url ) 
     dir.Append(
       Function(
         DirectoryItem( 
           ChannelDetails,
           name,
-          thumb=Function(GetThumb, thumbUrl=thumbUrl)
+          thumb=Function(GetThumb, thumb_url=thumb_url)
         ),
         url=url,
         name=name,
-        thumbUrl=thumbUrl
+        thumb_url=thumb_url
       )
     )
   return dir
   
 ###################################################################################################
-def ChannelDetails(sender,url,name,thumbUrl):
-  dir = MediaContainer(title1=PLUGIN_TITLE,title2=name,viewGroup='ChannelDetails')
+def ChannelDetails(sender,url,name,thumb_url):
+  dir = MediaContainer(title1=L('pokerstars.tv'),title2=name,viewGroup='ChannelDetails')
   # url = url.replace('-2.html', '-full-episodes.html')
   # Log( 'Getting episodes from: ' + BASE_URL + url )
   # episodes = HTML.ElementFromURL( BASE_URL + url ).xpath()
@@ -74,7 +72,7 @@ def ChannelDetails(sender,url,name,thumbUrl):
         DirectoryItem(
           ChannelVideos,
           section_name,
-          thumb=Function(GetThumb, thumbUrl=thumbUrl)
+          thumb=Function(GetThumb, thumb_url=thumb_url)
         ),
         url=url,
         channel_name=name,
@@ -93,19 +91,57 @@ def ChannelVideos(sender,url,channel_name,name):
       WebVideoItem(
         BASE_URL + video['url'],
         title=video['title'],
-        thumb=Function(GetThumb, thumbUrl=video['thumb_url'] + '?maxwidth=512&maxheight=512')
+        thumb=Function(GetThumb, thumb_url=video['thumb_url'] + '?maxwidth=512&maxheight=512')
       )
     )
     # Log( video['title'] )
     # Log( video['url'] )
     # Log( video['thumb_url'] )
   return dir
+  
+###################################################################################################
+def Spotlight(sender):
+  dir = MediaContainer(title1=L('pokerstars.tv'),title2=L('spotlight'),viewGroup='ChannelVideos')
+  highlights = HTML.ElementFromURL(SCHEDULE_URL, errors='ignore').xpath('//*/div[@id="template"]/div[@id="clm-two"]/div/div[@class="content spotlight"]/ul/li')
+    
+  for highlight in highlights:
+    title     = highlight.xpath('.//h3/a')[0].text.strip()
+    link      = highlight.xpath('.//a[@class="thumb"]')[0]
+    url       = link.get('href')
+    thumb_url = link.xpath('.//img')[0].get('src')
+    desc      = highlight.xpath('.//a[2]')[0].text.strip()
+    is_video  = ( url.find('poker-video') != -1 )
+    
+    if is_video:
+      dir.Append(
+        WebVideoItem(
+          BASE_URL + url,
+          title=title,
+          thumb=Function(GetThumb, thumb_url=thumb_url),
+          summary=desc
+        )
+      )
+    else:
+      dir.Append(
+        Function(
+          DirectoryItem( 
+            ChannelDetails,
+            title,
+            thumb=Function(GetThumb, thumb_url=thumb_url)
+          ),
+          url=url,
+          name=title,
+          thumb_url=thumb_url
+        )
+      )
+  
+  return dir
 
 ###################################################################################################
 # HELPERS
 ###################################################################################################
-def GetThumb(thumbUrl):
-  data = HTTP.Request(thumbUrl)
+def GetThumb(thumb_url):
+  data = HTTP.Request(thumb_url)
   if data:
     return DataObject(data, 'image/png')
   else:
@@ -114,7 +150,7 @@ def GetThumb(thumbUrl):
 def GetChannelVideos(url, is_sub_page=False ):
   videos = []
   the_url = BASE_URL + url
-  Log('Getting videos from:' + the_url)
+  # Log('Getting videos from:' + the_url)
   page = HTML.ElementFromURL(the_url, errors='ignore')
   video_elements = page.xpath( '//*/div[@id="clm-two"]/div[2]/div[@class="content clearfix"]/div[@class="results_vidList"]/ul[@class="videos"]/li/a' )
   for video in video_elements:
@@ -144,58 +180,3 @@ def GetChannelVideos(url, is_sub_page=False ):
         i = i + 1
 
   return videos
-# @handler('/video/pokerstars-tv', L('pokerstars.tv'))
-# def VideoMenu():
-#   dir = MediaContainer(viewGroup='Channels')
-#   dir.Append(DirectoryItem('foo','bar'))
-#   return dir
-#     # dir = MediaContainer(mediaType='video', viewGroup='Details')
-#     # episodes = JSON.ObjectFromURL(VIMCASTS_FEED_URL)['episodes']
-#     # episodes.reverse()  # Newest first
-#     # for episode in episodes:
-#     #     try:
-#     #         url     = episode['quicktime']['url']
-#     #         title   = F('episode', episode['episode_number'], episode['title'])
-#     #         date    = parsedate(episode['published_at'])
-#     #         date    = datetime(*date[:6])
-#     #         summary = dehtmlize(episode['abstract'].strip())
-#     #         thumb   = episode['poster']
-#     #         dir.Append(VideoItem(url, title=title, summary=summary, thumb=thumb,
-#     #                              subtitle=date.strftime('%A, %B %e %Y')))
-#     #     except AttributeError:
-#     #         Log("Something odd with episode, skipping: %s" %
-#     #                 episode, debugOnly=False)
-#     # return dir
-
-##
-# Removes HTML tags from a text string and converts character entities.
-# Adapted from:
-# http://effbot.org/zone/re-sub.htm#strip-html
-#
-# @param text The HTML source.
-# @return The plain text.  If the HTML source contains non-ASCII
-#     entities or character references, this is a Unicode string.
-# def dehtmlize(text):
-#     def convert_entities(m):
-#         text = m.group(0)
-#         if text[:2] == "&#":
-#             try:
-#                 if text[:3] == "&#x":
-#                     return unichr(int(text[3:-1], 16))
-#                 else:
-#                     return unichr(int(text[2:-1]))
-#             except ValueError:
-#                 pass
-#         elif text[:1] == "&":
-#             entity = htmlentitydefs.entitydefs.get(text[1:-1])
-#             if entity:
-#                 if entity[:2] == "&#":
-#                     try:
-#                         return unichr(int(entity[2:-1]))
-#                     except ValueError:
-#                         pass
-#                 else:
-#                     return unicode(entity, "iso-8859-1")
-#         return text # leave as is
-#     return String.StripTags(re.sub("&#?\w+;", convert_entities, text))
-
